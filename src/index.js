f99b5c4762bc0e8dff9a7efadec40a24c923ec0d
yash7xm <yash.7xm@gmail.com>
2024-02-16 13:16:53 +0530

    basic routes added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const { ServerConfig } = require("./config");
 
 const app = express();
 
+const apiRoutes = require('./routes');
+
+app.use("/api", apiRoutes);
+
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server Running on port ${ServerConfig.PORT}`);
 });
