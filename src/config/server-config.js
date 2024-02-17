74e84f2ffb1a3663cf72b9ec2a288f22eef336d1
yash7xm <yash.7xm@gmail.com>
2024-02-17 12:27:28 +0530

    Proxy added

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -2,14 +2,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const SALT_ROUNDS = process.env.SALT_ROUNDS;
-const JWT_EXPIRY = process.env.JWT_EXPIRY;
-const JWT_SECRET = process.env.JWT_SECRET;
-
 module.exports = {
-  PORT: PORT,
-  SALT_ROUNDS: SALT_ROUNDS,
-  JWT_SECRET: JWT_SECRET,
-  JWT_EXPIRY: JWT_EXPIRY,
+  PORT: process.env.PORT,
+  SALT_ROUNDS: process.env.SALT_ROUNDS,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
+  BOOKING_SERVICE: process.env.BOOKING_SERVICE
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const { ServerConfig } = require("./config");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
-  max: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes)
+  max: 30, // Limit each IP to 3 requests per `window` (here, per 15 minutes)
 });
 
 const apiRoutes = require("./routes");
@@ -15,6 +16,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
+
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightsService": "/" },
+  })
+);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/bookingService": "/" },
+  })
+);
 
 app.use("/api", apiRoutes);
 
