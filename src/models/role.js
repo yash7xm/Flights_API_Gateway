1a8596dd9f5f67d26fe24c6006e1c1a888ae7c5a
yash7xm <yash.7xm@gmail.com>
2024-02-17 13:01:42 +0530

    seeds for roles added

--- src/models/role.js
+++ src/models/role.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.User, { through: "User_Roles", as: "user" });
     }
   }
   Role.init(
